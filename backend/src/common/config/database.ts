244ed187bbf9f9656ee162fc57c5ac5f7c450218
Samuel Ogboye <ogboyesam@gmail.com>
2024-11-23 23:13:58 +0100

    chore: file restructure

--- backend/src/common/config/database.ts
+++ backend/src/common/config/database.ts
@@ -10,12 +10,12 @@ interface CustomConnectOptions extends ConnectOptions {
 
 export const connectDb = async (): Promise<void> => {
 	try {
-		const conn = await mongoose.connect(ENVIRONMENT.DB.URL, {
-			// your custom options go here
-		} as CustomConnectOptions);
+		// const conn = await mongoose.connect(ENVIRONMENT.DB.URL, {} as CustomConnectOptions);
+		const conn = await mongoose.connect('mongodb://root:example@mongo:27017/conferlink', {} as CustomConnectOptions);
 
 		console.log('MongoDB Connected to ' + conn.connection.name);
 	} catch (error) {
+		console.log(error);
 		console.log('Error: ' + (error as Error).message);
 		process.exit(1);
 	}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-black">ConferLink</h1>
+    </>
+  );
+}
+
+export default App;
