25d6ac6b2992b2bae6680ce6d33447a2e268e105
Samuel Ogboye <ogboyesam@gmail.com>
2024-11-24 17:57:47 +0100

    fix: fix tsconfig for frontend and backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // Specify output directory as "build"
+    outDir: "build", // Specify output directory as "build"
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
   },
 });
