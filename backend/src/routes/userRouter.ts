c5834d845cf91d544a7c415525cb544d8e014c60
Samuel Ogboye <ogboyesam@gmail.com>
2024-12-08 13:36:55 +0100

    refactor: update in user router

--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -1,8 +1,11 @@
-import { getUsers } from '@/controllers';
+import { getUser, getUsers, updateUser } from '@/controllers';
+import { authenticate } from '@/middlewares/validateAuth';
 import express from 'express';
 
 const router = express.Router();
 
 router.get('/', getUsers);
+router.get('/:id', getUser);
+router.put('/:id', updateUser);
 
 export { router as userRouter };
