226cab4c6a213d33bbf309a25f5f3566b2e3c129
Samuel Ogboye <ogboyesam@gmail.com>
2025-01-17 03:43:40 +0100

    refact: increate rate limit to 100

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -60,7 +60,7 @@ app.use(compression());
 // Rate limiter middleware
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 10, // Limit each IP to 100 requests per windowMs
+	max: 100, // Limit each IP to 100 requests per windowMs
 	message: 'Too many requests from this IP, please try again later.',
 });
 app.use(limiter);
