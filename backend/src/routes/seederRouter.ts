f211ae9acffb0137ab1f75a86404dfcc3c40aca1
Samuel Ogboye <ogboyesam@gmail.com>
2025-01-27 23:49:31 +0100

    refactor: add authenticate middleware to user route

--- backend/src/routes/seederRouter.ts
+++ backend/src/routes/seederRouter.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import { seedUsers } from '@/controllers/seeders/userSeeder';
+import { authenticate } from '@/middlewares/validateAuth';
 
 const router = express.Router();
 
-router.post('/user', seedUsers);
+router.post('/user', authenticate, seedUsers);
 
 export { router as seederRouter };
