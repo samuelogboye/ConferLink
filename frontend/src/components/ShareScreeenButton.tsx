4808ca0432f2b9de1aaddf54d5046ea2675c499b
Samuel Ogboye <ogboyesam@gmail.com>
2025-01-27 23:51:07 +0100

    feat(frontend): implement sharescreenbutton component

---
+++ frontend/src/components/ShareScreeenButton.tsx
@@ -0,0 +1,24 @@
+import { Button } from "./common/Button";
+
+export const ShareScreenButton: React.FC<{ onClick: () => void }> = ({
+    onClick,
+}) => {
+    return (
+        <Button className="p-4 mx-2" onClick={onClick}>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                />
+            </svg>
+        </Button>
+    );
+};
