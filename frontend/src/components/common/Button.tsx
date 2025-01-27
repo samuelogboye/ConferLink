f3b62ae140cc9072dbd4c356f1651aeeca13ddec
Samuel Ogboye <ogboyesam@gmail.com>
2025-01-27 23:52:15 +0100

    feat(frontend): implement button component

---
+++ frontend/src/components/common/Button.tsx
@@ -0,0 +1,30 @@
+import classNames from "classnames";
+
+interface ButtonProps {
+    onClick?: () => void;
+    className: string;
+    testId?: string;
+    type?: "submit" | "button" | "reset";
+    children: React.ReactNode;
+}
+export const Button: React.FC<ButtonProps> = ({
+    children,
+    onClick,
+    testId,
+    className,
+    type = "submit",
+}) => {
+    return (
+        <button
+            type={type}
+            data-testid={testId}
+            onClick={onClick}
+            className={classNames(
+                "bg-rose-400 p-2 rounded-lg hover:bg-rose-600 text-white",
+                className
+            )}
+        >
+            {children}
+        </button>
+    );
+};
