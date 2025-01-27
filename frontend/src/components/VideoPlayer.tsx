46ca74134b639d7307839e8c72569ef121221821
Samuel Ogboye <ogboyesam@gmail.com>
2025-01-27 23:51:16 +0100

    feat(frontend): implement video player component

---
+++ frontend/src/components/VideoPlayer.tsx
@@ -0,0 +1,18 @@
+import { useEffect, useRef } from "react";
+
+export const VideoPlayer: React.FC<{ stream?: MediaStream }> = ({ stream }) => {
+    const videoRef = useRef<HTMLVideoElement>(null);
+
+    useEffect(() => {
+        if (videoRef.current && stream) videoRef.current.srcObject = stream;
+    }, [stream]);
+    return (
+        <video
+            data-testid="peer-video"
+            style={{ width: "100%" }}
+            ref={videoRef}
+            autoPlay
+            muted={true}
+        />
+    );
+};
